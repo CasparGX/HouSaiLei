9ab796cdcf3ef063eaf90cf949d7fdaacb55af48
CasparGX <630248976@qq.com>
2016-03-14 14:57:50 +0800

    Update

--- app/src/main/java/com/casparx/housailei/CameraActivity.java
+++ app/src/main/java/com/casparx/housailei/CameraActivity.java
@@ -1,6 +1,7 @@
 package com.casparx.housailei;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.ImageFormat;
@@ -21,6 +22,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -45,7 +48,7 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
             String photoDir = Environment.getExternalStorageDirectory() + "/" + Environment.DIRECTORY_DCIM + "/Camera/";
             File tempFile = new File(photoDir + DateFormat.format("yyyy-MM-dd kk.mm.ss", System.currentTimeMillis())
                     .toString() + ".jpg");
-            Log.i(photoDir,tempFile+"");
+            Log.i(photoDir,bytes.length+"");
             try {
                 FileOutputStream fos = new FileOutputStream(tempFile);
                 fos.write(bytes);
@@ -64,6 +67,7 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
         FileInputStream fis = null;
         try {
             fis = new FileInputStream(path);
+
             Matrix matrix = new Matrix();
             matrix.setRotate(90);
             Bitmap bitmap = BitmapFactory.decodeStream(fis);
@@ -71,7 +75,8 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
             imgPhoto.setVisibility(View.VISIBLE);
             layoutCamera.setVisibility(View.GONE);
             //Bitmap bitmap = BitmapFactory.decodeFile(path);
-            imgPhoto.setImageBitmap(bitmap);
+            //imgPhoto.setImageBitmap(bitmap);
+            imgPhoto.setImageBitmap(readBitMap(this,fis));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -81,9 +86,13 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
     void onClickBtnTakePhoto() {
         Camera.Parameters parameters = mCamera.getParameters();
         parameters.setPictureFormat(ImageFormat.JPEG);
-        parameters.setPreviewSize(1920, 1080);
+        Camera.Size s = getBestSupportedSize(parameters.getSupportedPreviewSizes());
+        parameters.setPreviewSize(s.width, s.height);
+        s = getBestSupportedSize(parameters.getSupportedPictureSizes());
+        parameters.setPictureSize(s.width,s.height);
         parameters.setJpegQuality(100);
         parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+        mCamera.setParameters(parameters);
         mCamera.autoFocus(new Camera.AutoFocusCallback() {
             @Override
             public void onAutoFocus(boolean success, Camera camera) {
@@ -94,6 +103,21 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
         });
     }
 
+    /**
+     * 以最省内存的方式读取本地资源的图片
+     * @param context
+     * @param is
+     * @return
+     */
+    public static Bitmap readBitMap(Context context, InputStream is){
+        BitmapFactory.Options opt = new BitmapFactory.Options();
+        opt.inPreferredConfig = Bitmap.Config.RGB_565;
+        opt.inPurgeable = true;
+        opt.inInputShareable = true;
+        //获取资源图片
+        //InputStream is = context.getResources().openRawResource(resId);
+        return BitmapFactory.decodeStream(is,null,opt);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -180,5 +204,25 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback {
             mCamera.release();
             mCamera = null;
         }
+    }
+
+    /******************************************]
+     *
+     * 穷举法找出具有最大数目像素的尺寸
+     *
+     * @param sizes
+     * @return
+     */
+    public Camera.Size getBestSupportedSize(List<Camera.Size> sizes) {
+        Camera.Size bestSize = sizes.get(0);
+        int largestArea = bestSize.width*bestSize.height;
+        for (Camera.Size s :sizes) {
+            int area =s.width*s.height;
+            if (area>largestArea) {
+                bestSize=s;
+                largestArea = area;
+            }
+        }
+        return bestSize;
     }
 }
